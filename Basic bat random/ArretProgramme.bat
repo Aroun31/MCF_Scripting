f483a0dd2f0b32e0fdcc7d039cd02d46526655aa
Aroun Le BriCodeur <mc.family3531@gmail.com>
2018-06-14 00:26:47 +0200

    Update ArretProgramme.bat

--- Basic bat random/ArretProgramme.bat
+++ Basic bat random/ArretProgramme.bat
@@ -1,4 +1,4 @@
 TIMEOUT /T 1800 /NOBREAK
-echo "arrêt du système ! Bonne journée / soirée ;) "
-echo "arrêt du système dans : "
+echo arrêt du système ! Bonne journée / soirée ;) %username%
+echo arrêt du système dans : 
 shutdown -s -t 120
