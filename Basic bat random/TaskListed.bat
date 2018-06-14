4cc1760e455b264d91520e0719ddf6284077859a
Aroun Le BriCodeur <mc.family3531@gmail.com>
2018-06-14 20:22:21 +0200

    Update TaskListed.bat

--- Basic bat random/TaskListed.bat
+++ Basic bat random/TaskListed.bat
@@ -8,8 +8,8 @@ echo salut %username%
 echo affichage de la liste de tes processus
 echo Selectionne le PID de la tache qu'il te faut kill et tu fais :
 echo taskkill PID 
-echo exemple : taskkill /PID 3400 
-echo exemple : taskkill /PID 3400 /PID 9400 
+echo exemple : taskkill /F /T /PID 2948
+echo exemple : taskkill /F /T /PID 2948 /PID 3400  
 echo ouvrez un nouveau cmd pour faire la commande
 echo chargement des processus dans (appuie sur une touche les afficher tout de suite):
 TIMEOUT /T 120
