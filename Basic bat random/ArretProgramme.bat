298f489ff0144907b9f8aea41004f53ebc4658a2
Aroun Le BriCodeur <mc.family3531@gmail.com>
2019-03-14 05:28:32 +0100

    Update ArretProgramme.bat
    ajout de la récupération de la valeur dans la console
    conversion de la valeur minutes en secondes.

--- Basic bat random/ArretProgramme.bat
+++ Basic bat random/ArretProgramme.bat
@@ -1,4 +1,25 @@
-TIMEOUT /T 1800 /NOBREAK
-echo arrêt du système ! Bonne journée / soirée ;) %username%
-echo arrêt du système dans : 
-shutdown -s -t 120
+@echo off
+chcp 65001
+title Arrêt programmé du PC by Aroun Le BriCodeur
+color b
+:: 1heure = 3600 / 30min= 1800 / 5min=300 / 45min= 2700
+cls
+@echo off
+echo Hey ! Bienvenue sur le programme d'arrêt planifié de Aroun.
+set /p TimeShut="indique une valeur en minute ( exemple : 1, 10, 60, etc... ) : "
+set "ShutSeconde=120"
+set /a timeConvert=%TimeShut%*60
+cls
+echo Tu peux maintenant partir tranquillement te coucher je m'occupe d'éteindre le PC.
+echo bonne journée ou bonne nuit ;) 
+echo -
+echo -
+echo -
+echo arrêt à la fin du compte à rebours :
+if %username% == mcfam (
+    echo à bientôt sur le PC de Aroun
+) else (
+    echo à bientôt sur le PC de %username%
+)
+TIMEOUT /T %timeConvert% /NOBREAK
+shutdown -s -t %ShutSeconde%
