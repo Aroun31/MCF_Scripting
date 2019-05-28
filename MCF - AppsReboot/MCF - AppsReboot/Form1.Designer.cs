30d478242b67b12e668284a48de2665604f20b63
Aroun Le BriCodeur <mc.family3531@gmail.com>
2019-05-28 03:39:07 +0200

    Reboot PC
    Simple application permettant de programmer un arrêt du PC.
    Application faite pendant mon apprentissage au C# .

---
+++ MCF - AppsReboot/MCF - AppsReboot/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace MCF___AppsReboot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.EditHeure = new System.Windows.Forms.TextBox();
+            this.EditMinute = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Heure :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(315, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Minute :";
+            // 
+            // EditHeure
+            // 
+            this.EditHeure.Location = new System.Drawing.Point(101, 53);
+            this.EditHeure.Name = "EditHeure";
+            this.EditHeure.Size = new System.Drawing.Size(60, 20);
+            this.EditHeure.TabIndex = 1;
+            this.EditHeure.Text = "0";
+            // 
+            // EditMinute
+            // 
+            this.EditMinute.Location = new System.Drawing.Point(382, 52);
+            this.EditMinute.Name = "EditMinute";
+            this.EditMinute.Size = new System.Drawing.Size(60, 20);
+            this.EditMinute.TabIndex = 2;
+            this.EditMinute.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(146, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(197, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Arrêt du PC programmé";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(205, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 20);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Arrêt à  :";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(15, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Valider";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(339, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 41);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Annuler";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(100)))));
+            this.ClientSize = new System.Drawing.Size(489, 184);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.EditMinute);
+            this.Controls.Add(this.EditHeure);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MCF - AppsRebbot by Aroun";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox EditHeure;
+        private System.Windows.Forms.TextBox EditMinute;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ MCF - AppsReboot/MCF - AppsReboot/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace MCF___AppsReboot
+{
+    public partial class Form1 : Form
+    {
+        Int32 heure;
+        Int32 minute;
+        int resultat;
+        bool valideShutdown;
+        string msg;
+        int newHeure;
+        int newMinute;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime actualDate = DateTime.Now;
+            string[] split = actualDate.ToString().Split(new Char[] { ' ', '/' });
+            string actualHeure = split[3];
+            string[] NewSplit = actualHeure.Split(':');
+            newHeure = Convert.ToInt32(NewSplit[0]);
+            newMinute = Convert.ToInt32(NewSplit[1]);
+            valideShutdown = true;
+            msg = "Opération validé !\nMerci d'avoir urilisé :\nMCF - AppsRebbot";
+
+            if (EditHeure.Text.Trim() != "")
+            {
+                heure = Convert.ToInt32(EditHeure.Text.Trim());
+            }
+            if (EditMinute.Text.Trim() != "")
+            {
+                minute = Convert.ToInt32(EditMinute.Text.Trim());
+            }
+
+            if (minute != 0)
+            {
+                if (minute >= 60)
+                {
+                    valideShutdown = false;
+                    msg = "Opération impossible !\nMerci de renseigner les minutes entre 0 et 59";
+                }
+                else
+                {
+                    newMinute += minute;
+                    if (newMinute > 60)
+                    {
+                        newHeure ++;
+                        newMinute = newMinute -= 60;
+                    }
+                    minute = minute * 60;
+                }
+            }
+
+            if (heure != 0)
+            {
+                newHeure += heure;
+                heure = heure * 60;
+                heure = heure * 60;
+            }
+
+            MessageBox.Show(msg, "MCF - AppsRebbot");
+            if (valideShutdown)
+            {
+                resultat = heure + minute;
+                string heureShutdown = "Arrêt à : \n" + newHeure + "H" + newMinute + "Min";
+                label5.Text = heureShutdown;
+                Process.Start("shutdown", "/s /t " + resultat);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (valideShutdown)
+            {
+                Process.Start("shutdown", "/a");
+                MessageBox.Show("Vous venez d'annuler l'arrêt programmé.");
+                valideShutdown = false;
+            }
+            else
+            {
+                MessageBox.Show("Aucun arrêt programmé n'est en cours");
+            }
+
+        }
+    }
+}
